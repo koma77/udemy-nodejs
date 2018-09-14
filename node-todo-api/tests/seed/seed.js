8467d1b8a1ff71f3b0b32785347102d61566cfcf
Andrey <1r2d2@keemail.me>
2018-09-14 15:22:00 +0400

    Refactor config

--- node-todo-api/tests/seed/seed.js
+++ node-todo-api/tests/seed/seed.js
@@ -40,7 +40,7 @@ const users = [{
     password: 'userOnePass',
     tokens: [{
         access: 'auth',
-        token: jwt.sign({_id: userOneID, access: 'auth'}, 'abc123').toString()
+        token: jwt.sign({_id: userOneID, access: 'auth'}, process.env.JWT_SECRET).toString()
     }]
 }, {
     _id: userTwoID,
@@ -48,7 +48,7 @@ const users = [{
     password: 'userTwoPassword',
     tokens: [{
         access: 'auth',
-        token: jwt.sign({_id: userTwoID, access: 'auth'}, 'abc123').toString()
+        token: jwt.sign({_id: userTwoID, access: 'auth'}, process.env.JWT_SECRET).toString()
     }]
 }]
 
